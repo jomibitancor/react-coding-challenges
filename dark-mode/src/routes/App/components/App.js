54f1ccb18d5a169d601d696dda7b164320734e4c
jbitancor <jbitancor@pygo.io>
2021-10-10 09:13:43 -0700

    dark mode complete

--- dark-mode/src/routes/App/components/App.js
+++ dark-mode/src/routes/App/components/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 import '../styles/_app.scss';
 
 function App() {
+    const [themeMode, setThemeMode] = useState(true)
+
   return (
-    <div className="app">
+    <div className={themeMode ? 'app' : 'app dark-mode'}>
       <div className="level">
         <div>
           <h1 className="title">Dark Mode Challenge</h1>
         </div>
 
         {/* --The button that should toggle dark mode-- */}
-        <button className="app__dark-mode-btn icon level-right">
-          <FontAwesomeIcon icon={faMoon} />
+        <button onClick={()=>{setThemeMode(!themeMode)}} className="app__dark-mode-btn icon level-right">
+          <FontAwesomeIcon color={themeMode ? "" : "#FFA500"} icon={themeMode ? faMoon : faSun} />
         </button>
 
       </div>
