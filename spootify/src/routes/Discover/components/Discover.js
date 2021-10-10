ade4df9dbac41b13582d04cdb3445fe5498bfac4
jbitancor <jbitancor@pygo.io>
2021-10-10 13:29:52 -0700

    spootify complete

--- spootify/src/routes/Discover/components/Discover.js
+++ spootify/src/routes/Discover/components/Discover.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import DiscoverBlock from './DiscoverBlock/components/DiscoverBlock';
-import '../styles/_discover.scss';
+import React, { Component } from "react";
+import axios from "axios";
+import qs from "qs";
+import DiscoverBlock from "./DiscoverBlock/components/DiscoverBlock";
+import config from "../../../config";
+import "../styles/_discover.scss";
 
 export default class Discover extends Component {
   constructor() {
@@ -11,6 +14,61 @@ export default class Discover extends Component {
       playlists: [],
       categories: []
     };
+  }
+
+  componentDidMount() {
+    const b64encoded_clientCreds = btoa(
+      config.api.clientId + ":" + config.api.clientSecret
+    );
+    const params = qs.stringify({
+      grant_type: "client_credentials",
+    });
+    let token = "";
+
+    // Get Token
+    axios({
+      method: "post",
+      url: "https://accounts.spotify.com/api/token",
+      headers: {
+        Authorization: " Basic " + b64encoded_clientCreds,
+      },
+      data: params,
+    })
+      .then((res) => {
+        token = res.data.access_token;
+        const header = { Authorization: "Bearer " + token };
+
+        // New Releases
+        axios({
+          url: "https://api.spotify.com/v1/browse/new-releases",
+          headers: header,
+        }).then((r) => {
+          this.setState({
+            newReleases: r.data.albums.items,
+          });
+        });
+        // Playlists
+        axios({
+          url: "https://api.spotify.com/v1/browse/featured-playlists",
+          headers: header,
+        }).then((r) => {
+          this.setState({
+            playlists: r.data.playlists.items,
+          });
+        });
+        // Categories
+        axios({
+          url: "https://api.spotify.com/v1/browse/categories",
+          headers: header,
+        }).then((r) => {
+          this.setState({
+            categories: r.data.categories.items,
+          });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   render() {
